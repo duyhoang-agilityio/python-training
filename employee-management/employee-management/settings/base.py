eb2fb43a4b68fe9e8d620fc3186dbbab5e9c28d5
Duy Hoang <duy.hoang@asnet.com.vn>
2025-02-25 15:58:01 +0700

    custom the exception handler

--- employee-management/employee-management/settings/base.py
+++ employee-management/employee-management/settings/base.py
@@ -79,6 +79,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
+    "EXCEPTION_HANDLER": "employee.utils.custom_exception_handler",
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
         "rest_framework.authentication.SessionAuthentication",
---
+++ employee-management/employee/utils.py
@@ -0,0 +1,23 @@
+from rest_framework.views import exception_handler
+from rest_framework.response import Response
+from rest_framework import status
+
+
+def custom_exception_handler(exc, context):
+    # Call REST framework's default exception handler first,
+    # to get the standard error response.
+    response = exception_handler(exc, context)
+
+    if response is not None:
+        # Add the HTTP status code to the response.
+        response.data["status_code"] = response.status_code
+    else:
+        # Handle exceptions that weren't caught by DRF's default handler
+        response = Response(
+            {
+                "detail": "Internal Server Error",
+                "status_code": status.HTTP_500_INTERNAL_SERVER_ERROR,
+            },
+            status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
+    return response
